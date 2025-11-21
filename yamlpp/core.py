3f6fff6ceeaa0dfdc3e817acae7ec9af08f799d7
Laurent Franceschetti <36767124+fralau@users.noreply.github.com>
2025-11-21 14:38:48 +0100

    Improve reporting of errors
      - Errors messages contain construct name, line, and type of error
      - Introduced yamlp_schema, for external (binary) validation

--- yamlpp/core.py
+++ yamlpp/core.py
@@ -6,17 +6,23 @@ Core application for the YAMLpp interpreter
 
 import os
 from enum import Enum
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 from io import StringIO
 import ast
+
+
 
 from jinja2 import Environment, StrictUndefined
 from jinja2.exceptions import UndefinedError as Jinja2UndefinedError
 from super_collections import super_collect, SuperCollection, SuperDict
 
+
 from .stack import Stack
-from .util import yaml, load_yaml, dequote, get_line_number
+from .util import yaml, load_yaml, dequote, get_line_number, validate_node
 from .import_modules import get_exports
+
+
+
 
 # --------------------------
 # Language fundamentals
@@ -26,7 +32,7 @@ from .import_modules import get_exports
 BlockNode = Dict[str, Any]
 ListNode  = List[Any]
 Node = Union[BlockNode, ListNode, str, int, float, bool, None] 
-
+KeyOrIndexentry = Tuple[Union[str, int], Node]
 
 # Global functions for Jinja2
 GLOBAL_CONTEXT = {
@@ -34,7 +40,10 @@ GLOBAL_CONTEXT = {
 }
 
 
+
+
 class Error(str, Enum):
+    VALIDATION = "ValidationError"
     KEY = "KeyNotFound"
     INDEX = "IndexNotFound"
     ARGUMENTS = "ArgumentMismatch"
@@ -54,20 +63,55 @@ class YAMLppError(Exception):
 
     def __str__(self):
         return (f"[{self.err_type}] Line {self.line_no}: {self.message}")
+
+class MappingEntry:
+    """
+    A key value entry
+    """
+
+    def __init__(self, key:str, value: Node):
+        "Initialize"
+        self.key = key
+        self.value = value
+
+    @property
+    def attributes(self):
+        """
+        Get the attributes of the current entry.
+        It works only on a dictionary value.
+        """
+        try:
+            return list(self.value.keys())
+        except AttributeError:
+            raise ValueError("This mapping entry does not have attribues")
+
+    def get(self, key:str|int, err_msg:str=None, strict:bool=False) -> Node:
+        """
+        Get a child from a node by key, and raise an error if not found.
+        The value of entry must be either dict (it's an attribute) or list.
+        """
+        if not isinstance(self.value, (list, dict)):
+            raise YAMLppError(f"Key {self.key} points on a scalar or non-recognized type ({type(self.value).__name__})")
+        try:
+            return self.value[key]
+        except (KeyError, IndexError):
+            if strict:
+                if err_msg is None:
+                    if isinstance(key, str):
+                        err_msg = f"Structure '{self.key}' does not contain '{key}'"
+                    elif isinstance(key, int):
+                        err_msg = f"Sequence in '{self.key}' does not contain {key}nth element"
+                raise YAMLppError(self.value, Error.KEY, err_msg)
+            else:
+                return None
+            
+    def __getitem__(self, key):
+        "Same semantics as a dict or list"
+        return self.get(key, strict=True)
     
-def get(block: dict|list, index:str|int, err_msg:str="Item not found"):
-    """
-    Get a value from a node, and raise an error if not found.
-    The block must be either dict or list.
-    """
-    if not isinstance(block, (dict, list)):
-        return YAMLppError("Block is not dict or list")
-    try:
-        return block[index]
-    except KeyError:
-        raise YAMLppError(block, Error.KEY, err_msg)
-    except IndexError:
-        raise YAMLppError(block, Error.KEY, err_msg)
+    def __str__(self):
+        "Print the entry"
+        return(f"{self.key} ->\n{Interpreter.to_yaml(self.value)}")
 
 # --------------------------
 # Interpreter
@@ -84,24 +128,35 @@ class Interpreter:
         if not source_dir:
             # working directory
             self._source_dir = os.getcwd()
+
         if filename:
             self.load(filename)
+        
+
+
+    def load(self, filename:str, validate:bool=False):
+        "Load a YAMLpp file"
+        self._source_dir = os.path.dirname(filename)
+        self._yamlpp, self._initial_tree = load_yaml(filename)
+        if validate:
+            validate_node(self._initial_tree)
+        self._reset_environment()
+
+
+    def _reset_environment(self):
+        "Reset the Jinja environment"
         # create the interpretation environment
         # variables not found will raise an error
-        self._jinja2_env = env = Environment(undefined=StrictUndefined)
+        self._jinja_env = env = Environment(undefined=StrictUndefined)
         env.globals = Stack(env.globals)
         assert isinstance(env.globals, Stack)
         env.globals.push(GLOBAL_CONTEXT)
         env.filters = Stack(env.filters)
         assert isinstance(env.filters, Stack)
 
-    def load(self, filename:str):
-        "Load a YAMLpp file"
-        self._yamlpp, self._initial_tree = load_yaml(filename)
-        self._source_dir = os.path.dirname(filename)
-
-
-
+    # -------------------------
+    # Properties
+    # -------------------------
     @property
     def initial_tree(self):
         "Return the initial tree (Ruamel)"
@@ -124,15 +179,15 @@ class Interpreter:
         return self._yamlpp
     
     @property
-    def jinja2_env(self):
-        "The jinja2 environment"
-        return self._jinja2_env
+    def jinja_env(self):
+        "The jinja environment"
+        return self._jinja_env
     
     @property
     def stack(self):
-        "The contextual Jinja2 stack containing the values"
+        "The contextual Jinja stack containing the values"
         # return self._stack
-        return self.jinja2_env.globals
+        return self.jinja_env.globals
     
     @property
     def source_dir(self) -> str:
@@ -213,7 +268,7 @@ class Interpreter:
         """
         if not isinstance(expr, str):
             expr = repr(expr)
-        template = self.jinja2_env.from_string(expr)
+        template = self.jinja_env.from_string(expr)
         # return template.render(**self.stack)
         r = template.render()
         # print("Evaluate", expr, "->", r, ">", type(r).__name__)
@@ -247,7 +302,9 @@ class Interpreter:
         """
         # print("*** Type:", node, "***", type(node).__name__)
         # assert isinstance(self.stack, Stack), f"The stack is not a Stack but '{type(self.stack).__name__}':\n{node}'"
-        if isinstance(node, str):
+        if node is None:
+            return None;
+        elif isinstance(node, str):
             # String
             try:
                 return self.evaluate_expression(node)
@@ -264,34 +321,34 @@ class Interpreter:
             if params_block:
                 new_scope = self.get_scope(params_block)
                 self.stack.push(new_scope)
-                self.jinja2_env.filters.push({})
+                self.jinja_env.filters.push({})
             
             result_dict:dict = {}
             result_list:list = []
             for key, value in node.items():
-                # print("Key/Value:", key, value)
+                entry = MappingEntry(key, value)
                 if key == ".context":
                     # Do not include
                     r = None
                 elif key == ".do":
-                    r = self.handle_do(value)
+                    r = self.handle_do(entry)
                 elif key == ".foreach":
-                    r = self.handle_foreach(value)
+                    r = self.handle_foreach(entry)
                     # print("Returned foreach:",)
                 elif key == ".switch":
-                    r = self.handle_switch(value)
+                    r = self.handle_switch(entry)
                 elif key == ".if":
-                    r = self.handle_if(value)
+                    r = self.handle_if(entry)
                 elif key == ".import":
-                    r = self.handle_import(value)
+                    r = self.handle_import(entry)
                 elif key == ".module":
-                    r = self.handle_module(value)
+                    r = self.handle_module(entry)
                 elif key == ".function":
-                    r = self.handle_function(value)
+                    r = self.handle_function(entry)
                 elif key == ".call":
-                    r = self.handle_call(value)
+                    r = self.handle_call(entry)
                 elif key == ".export":
-                    r = self.handle_export(value)
+                    r = self.handle_export(entry)
                 else:
                     # normal YAML key
                     r = {key: self.process_node(value)}
@@ -309,7 +366,7 @@ class Interpreter:
             if params_block:
                 # end of the scope, for these parameters
                 self.stack.pop()
-                self.jinja2_env.filters.pop()
+                self.jinja_env.filters.pop()
 
             if len(result_dict):
                 return result_dict
@@ -332,17 +389,17 @@ class Interpreter:
     # Specific handlers (after dispatcher)
     # -------------------------
 
-    def handle_do(self, sequence: ListNode) -> ListNode:
+    def handle_do(self, entry:MappingEntry) -> ListNode:
         """
         Sequence of instructions
         """
         print(f"*** DO action ***")
         results: ListNode = []
-        for item in sequence:
-            results.append(self.process_node(item))
+        for node in entry.value:
+            results.append(self.process_node(node))
         return results
 
-    def handle_foreach(self, block: Dict[str, Any]) -> List[Any]:
+    def handle_foreach(self, entry:MappingEntry) -> List[Any]:
         """
         Loop
 
@@ -352,7 +409,7 @@ class Interpreter:
         }
         """
         # print("\nFOREACH")
-        var_name, iterable_expr = get(block, ".values")
+        var_name, iterable_expr = entry[".values"]
         result = self.evaluate_expression(iterable_expr)
         # the result was a string; it needs to be converted:
         # iterable = dequote(result)
@@ -363,13 +420,13 @@ class Interpreter:
             local_ctx = {}
             local_ctx[var_name] = item
             self.stack.push(local_ctx)
-            do = get(block, ".do")
+            do = entry[".do"]
             results.append(self.process_node(do))
             self.stack.pop()
         return results
 
 
-    def handle_switch(self, block: Dict[str, Any]) -> Optional[Any]:
+    def handle_switch(self, entry:MappingEntry) -> Node:
         """
         block = {
             ".expr": "...",
@@ -377,16 +434,16 @@ class Interpreter:
             ".default": [...]
         }
         """
-        expr_value = self.evaluate_expression(block[".expr"])
-        cases: Dict[Any, Any] = block.get(".cases", {})
+        expr = entry[".expr"]
+        expr_value = self.evaluate_expression(expr)
+        cases: Dict[Any, Any] = entry[".cases"]
         if expr_value in cases:
             return self.process_node(cases[expr_value])
-        elif ".default" in block:
-            return self.process_node(block[".default"])
-        return None
+        else:
+            return self.process_node(cases.get(".default"))
 
 
-    def handle_if(self, block: Dict[str, Any]) -> Any:
+    def handle_if(self, entry:MappingEntry) -> Node:
         """
         And if then else structure
 
@@ -396,43 +453,43 @@ class Interpreter:
             ".else": [...]
         }
         """
-        r = self.evaluate_expression(block['.cond'])
+        r = self.evaluate_expression(entry['.cond'])
         # transform the Jinja2 string into a value that can be evaluated
         # condition = dequote(r)
         condition = r
         if condition:
-            r = self.process_node(block['.then'])
+            r = self.process_node(entry['.then'])
         else:
-            r = self.process_node(block.get(".else", ""))
+            r = self.process_node(entry.get(".else"))
         # print("handle_if:", r)
         return r
 
 
 
-    def handle_import(self, value:str) -> Any:
+    def handle_import(self, entry:MappingEntry) -> Node:
         """
         Import of an external file
         """
-        filename = self.evaluate_expression(value)
+        filename = self.evaluate_expression(entry.value)
         full_filename = os.path.join(self.source_dir, filename)
         _, data = load_yaml(full_filename)
         return self.process_node(data)
     
-    def handle_module(self, value:str) -> None:
+    def handle_module(self, entry:MappingEntry) -> None:
         """
         Import a Python module, with variables (function) and filters.
         The import is scoped.
         """
-        filename =  self.evaluate_expression(value)
+        filename =  self.evaluate_expression(entry.value)
         full_filename = os.path.join(self.source_dir, filename)
         variables, filters = get_exports(full_filename)
         # note how we use update(), since we add to the local scope:
-        self.jinja2_env.globals.update(variables)
-        self.jinja2_env.filters.update(filters)
+        self.jinja_env.globals.update(variables)
+        self.jinja_env.filters.update(filters)
         return None
 
     
-    def handle_function(self, block:BlockNode):
+    def handle_function(self, entry:MappingEntry) -> None:
         """
         Create a function
         A function is a block with a name, arguments and a sequence, which returns a subtree.
@@ -443,15 +500,15 @@ class Interpreter:
             ".do": [...]
         }
         """
-        name = get(block,'.name', "Function does not have a name")
+        name = entry['.name']
         print("Function created with its name!", name)
-        self.stack[name] = block
+        self.stack[name] = entry.value
         return None
 
         
         
 
-    def handle_call(self, block:BlockNode):
+    def handle_call(self, entry:MappingEntry) -> Node:
         """
         Call a function, with its arguments
         block = {
@@ -459,17 +516,18 @@ class Interpreter:
             ".args": {},
         }
         """
-        name = get(block,'.name')
+        name = entry['.name']
         # print(f"*** CALLING {name} ***")
         try:
-            function = self.stack[name]
+            function = MappingEntry(name, self.stack[name])
         except KeyError:
-            raise YAMLppError(block, Error.KEY, f"Function '{name}' not found!")
+            raise YAMLppError(entry, Error.KEY, f"Function '{name}' not found!")
         # assign the arguments
-        formal_args = get(function, '.args', "Call without arguments")
-        args = get(block,'.args')
+        
+        formal_args = function['.args']
+        args = entry['.args']
         if len(args) != len(formal_args):
-            raise YAMLppError(block, 
+            raise YAMLppError(entry, 
                               Error.ARGUMENTS,
                               f"No of arguments not matching, expected {len(formal_args)}, found {len(args)}")
         assigned_args = dict(zip(formal_args, args))
@@ -477,13 +535,14 @@ class Interpreter:
                
 
         # create the new block and copy the arguments as context
-        new_block = function['.do'].copy()
+        actions = function['.do']
+        new_block = actions.copy()
         assert not isinstance(new_block, SuperCollection)
         new_block['.context'] = assigned_args
         return self.process_node(new_block)
 
 
-    def handle_export(self, block: Dict[str, Any]) -> Any:
+    def handle_export(self, entry:MappingEntry) -> None:
         """
         Exports the subtree into an external file
 
@@ -492,14 +551,12 @@ class Interpreter:
             ".content": {...} or []
         }
         """
-        filename = self.evaluate_expression(get(block, '.filename'))
+        filename = self.evaluate_expression(entry['.filename'])
         full_filename = os.path.join(self.source_dir, filename)
-        tree = self.process_node(get(block, '.content'))
+        tree = self.process_node(entry['.content'])
         yaml_output = self.to_yaml(tree)
         with open(full_filename, 'w') as f:
             f.write(yaml_output)
-
-        return None
 
 
         
